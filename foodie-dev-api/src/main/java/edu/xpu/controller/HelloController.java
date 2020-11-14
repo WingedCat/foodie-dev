5c158e8b33efaf411ba33bd77cebcd668652ab77
huchengpeng <2233749193@qq.com>
2020-11-14 18:47:21 +0800

    初始化项目

---
+++ foodie-dev-api/src/main/java/edu/xpu/controller/HelloController.java
@@ -0,0 +1,13 @@
+package edu.xpu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello foodie-dev";
+    }
+}
